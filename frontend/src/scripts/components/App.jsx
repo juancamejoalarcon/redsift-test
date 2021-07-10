4c2a84a96137c17747819438b7559ab9504bc1a1
Juan Camejo Alarcón <juancamejo93@gmail.com>
2021-07-10 18:20:44 +0100

    added summary changes

--- frontend/src/scripts/components/App.jsx
+++ frontend/src/scripts/components/App.jsx
@@ -8,6 +8,7 @@ import { translate } from 'react-i18next';
 import Header from './Header';
 import Message from './Message';
 import Menu from './Menu';
+import { SummaryChart } from "./SummaryChart";
 
 class App extends Component {
   static propTypes = {
@@ -15,21 +16,53 @@ class App extends Component {
     data: PropTypes.any,
   }
 
+  getTotalValidMessages = (messages) => {
+    const total = {
+      DKIM: 0,
+      DMARC: 0,
+      SPF: 0
+    }
+    messages.forEach((m) => {
+      if (m.authResults) {
+        ['DKIM', 'DMARC', 'SPF'].forEach(value => {
+          if (m.authResults[value] === 'pass' || m.authResults[value] === 'bestguesspass') {
+            total[value]++
+          }
+        })
+      }
+    })
+    return total
+  }
+
   render() {
     const { t, data } = this.props;
     const { counts, messages } = data;
+    const totalValidMessages = this.getTotalValidMessages(messages)
+    const Comp = SummaryChart({
+      initData: [{
+        label: `DKIM (${totalValidMessages.DKIM})`,
+        value: totalValidMessages.DKIM
+      }, {
+        label: `DMARC (${totalValidMessages.DMARC})`,
+        value: totalValidMessages.DMARC
+      }, {
+        label: `SPF (${totalValidMessages.SPF})`,
+        value: totalValidMessages.SPF
+      }]
+    });
 
     return (
       <div>
-        <Header/>
-        <Menu/>
+        <Comp />
+        <Header />
+        <Menu />
         {/* <h1>{t('app:title-home')}</h1>
         <h4>{t('app:description-home', {count: counts.messageTotal})}</h4>
         { messages.map((m, key) => <div key={key}><p>{m.subject}</p></div>) } */}
         <div className="message-list">
           {messages.map((m, key) => <Message key={key} message={m} />)}
         </div>
-      </div>
+      </div >
     );
   }
 }
---
+++ frontend/src/scripts/components/SummaryChart.jsx
@@ -0,0 +1,52 @@
+import D3Wrap from "../libs/d3Wrap";
+import * as d3 from "d3";
+
+export const SummaryChart = ({initData}) => {
+    const methods = {
+        initialize: (div, data, options) => {
+            const w = 300;
+            const h = 300;
+            const r = 100;
+            const color =  d3.scaleOrdinal(d3.schemeCategory10);
+            const usedData = data || initData;
+            console.log("DTATA", usedData)
+            
+            const vis = d3.select(div)
+                .append("svg:svg")
+                .data([usedData])
+                    .attr("width", w)
+                    .attr("height", h)
+                .append("svg:g")
+                    .attr("transform", "translate(" + r + "," + r + ")")
+        
+            const arc = d3.arc()
+                .outerRadius(r).innerRadius(0);
+        
+            const pie = d3.pie()
+                .value(function(d) { return d.value; });
+        
+            const arcs = vis.selectAll("g.slice")
+                .data(pie)
+                .enter()
+                    .append("svg:g")
+                        .attr("class", "slice");
+
+        
+            arcs.append("svg:path")
+                    .attr("fill", function(d, i) { return color(i); } )
+                    .attr("d", arc);
+    
+            arcs.append("svg:text")
+                    .attr("transform", function(d) {
+                    d.innerRadius = 0;
+                    d.outerRadius = r;
+                    return "translate(" + arc.centroid(d) + ")";
+                })
+                .attr("text-anchor", "middle")
+                .text(function(d, i) { return usedData[i].label; });
+                
+        }
+    };
+
+    return D3Wrap(methods);
+}
